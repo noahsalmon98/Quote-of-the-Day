40f3b3df73db95ef515fc57c5ed3877e6589b903
mluron-ArxFjs <ms.luron@nycacademy.org>
2023-05-11 19:11:55 -0400

    google-translate

---
+++ assests/google-translate.js
@@ -0,0 +1,25 @@
+function googleTranslateElementInit() {
+    new google.translate.TranslateElement({ pageLanguage: 'en', layout: google.translate.TranslateElement.FloatPosition.TOP_LEFT }, 'google_translate_element');
+}
+
+function triggerHtmlEvent(element, eventName) {
+    var event;
+    if (document.createEvent) {
+        event = document.createEvent('HTMLEvents');
+        element.dispatchEvent(event);
+    } else {
+        event = document.createEventObject();
+        event.eventType = eventName;
+        element.fireEvent('on' + event.eventType, event);
+    }
+}
+
+jQuery('.lang-select').click(function () {
+    var theLang = jQuery(this).attr('data-lang');
+    jQuery('.goog-te-combo').val(theLang);
+
+    //alert(jQuery(this).attr('href'));
+    window.location = jQuery(this).attr('href');
+    location.reload();
+
+});
