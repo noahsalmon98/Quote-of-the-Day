a02d6ede3b1e9dace96a678ea060d5399b5502d4
ChaozWollf <128013056+ChaozWollf@users.noreply.github.com>
2023-05-15 19:05:55 -0500

    trying to get translation to work

--- assests/script.js
+++ assests/script.js
@@ -1,3 +1,27 @@
+const url = 'https://google-translate1.p.rapidapi.com/language/translate/v2/detect';
+const options = {
+    method: 'POST',
+    headers: {
+        'content-type': 'application/x-www-form-urlencoded',
+        'Accept-Encoding': 'application/gzip',
+        'X-RapidAPI-Key': 'da30ef9e92msh9064eb8ecf5ca2ep1640fdjsn765e66d2173f',
+        'X-RapidAPI-Host': 'google-translate1.p.rapidapi.com'
+    },
+    body: new URLSearchParams({
+        q: 'English is hard, but detectably so'
+    })
+};
+
+try {
+    const response = await fetch(url, options);
+    const result = await response.text();
+    console.log(result);
+} catch (error) {
+    console.error(error);
+}
+
+
+
 // Display today's date
 const todayDate = dayjs().format('MM/DD/YYYY');
 const dateEl = document.getElementById("currentDate");
