93f0c1d0988d001409e82bdb73ab83621a38ca1b
ChaozWollf <128013056+ChaozWollf@users.noreply.github.com>
2023-05-15 19:18:16 -0500

    tried to add favorite buttons

--- assests/script.js
+++ assests/script.js
@@ -1,25 +1,3 @@
-const url = 'https://google-translate1.p.rapidapi.com/language/translate/v2/detect';
-const options = {
-    method: 'POST',
-    headers: {
-        'content-type': 'application/x-www-form-urlencoded',
-        'Accept-Encoding': 'application/gzip',
-        'X-RapidAPI-Key': 'da30ef9e92msh9064eb8ecf5ca2ep1640fdjsn765e66d2173f',
-        'X-RapidAPI-Host': 'google-translate1.p.rapidapi.com'
-    },
-    body: new URLSearchParams({
-        q: 'English is hard, but detectably so'
-    })
-};
-
-try {
-    const response = await fetch(url, options);
-    const result = await response.text();
-    console.log(result);
-} catch (error) {
-    console.error(error);
-}
-
 
 
 // Display today's date
@@ -128,3 +106,25 @@ function transl() {
     
 
 }
+const url = 'https://google-translate1.p.rapidapi.com/language/translate/v2/detect';
+const options = {
+    method: 'POST',
+    headers: {
+        'content-type': 'application/x-www-form-urlencoded',
+        'Accept-Encoding': 'application/gzip',
+        'X-RapidAPI-Key': 'da30ef9e92msh9064eb8ecf5ca2ep1640fdjsn765e66d2173f',
+        'X-RapidAPI-Host': 'google-translate1.p.rapidapi.com'
+    },
+    body: new URLSearchParams({
+        q: 'English is hard, but detectably so'
+    })
+};
+
+try {
+    const response = await fetch(url, options);
+    const result = await response.text();
+    console.log(result);
+} catch (error) {
+    console.error(error);
+}
+
